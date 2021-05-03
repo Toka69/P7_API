c8b5d75fdff96f9fa565df2b71936fb047797673
Matthias LEROUX <matthiasleroux@laposte.net>
2021-05-03 21:38:07 +0200

    start fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,66 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        /**** Products ****/
+        $product1 = new Product();
+        $product1->setName()
+            ->setManufacturer()
+            ->setModel()
+            ->setDescription()
+            ->setPrice()
+            ->setTva()
+            ->setCreatedDate()
+            ->setUpdatedDate()
+            ;
+        $manager->persist($product1);
+
+        $product2 = new Product();
+        $product2->setName()
+            ->setManufacturer()
+            ->setModel()
+            ->setDescription()
+            ->setPrice()
+            ->setTva()
+            ->setCreatedDate()
+            ->setUpdatedDate()
+        ;
+        $manager->persist($product2);
+
+        $product3 = new Product();
+        $product3->setName()
+            ->setManufacturer()
+            ->setModel()
+            ->setDescription()
+            ->setPrice()
+            ->setTva()
+            ->setCreatedDate()
+            ->setUpdatedDate()
+        ;
+        $manager->persist($product3);
+
+        $product4 = new Product();
+        $product4->setName()
+            ->setManufacturer()
+            ->setModel()
+            ->setDescription()
+            ->setPrice()
+            ->setTva()
+            ->setCreatedDate()
+            ->setUpdatedDate()
+        ;
+        $manager->persist($product4);
+
+        $product5 = new Product();
+        $product5->setName()
+            ->setManufacturer()
+            ->setModel()
+            ->setDescription()
+            ->setPrice()
+            ->setTva()
+            ->setCreatedDate()
+            ->setUpdatedDate()
+        ;
+        $manager->persist($product5);
 
         $manager->flush();
     }
