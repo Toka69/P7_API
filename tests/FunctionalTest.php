8c39a6ad4684e76aef7eb32c9c72f64a4d6b8b7c
Matthias LEROUX <matthiasleroux@laposte.net>
2021-05-24 19:29:30 +0200

    Add a functional test to verify that an anonymous user can't access to products and users.

---
+++ tests/FunctionalTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Component\Panther\PantherTestCase;
+
+class FunctionalTest extends PantherTestCase
+{
+    public function testAnonymousUserCantAccessProducts():void
+    {
+        $client = static::createPantherClient();
+        $crawler = $client->request('GET', '/api/products');
+        $this->assertStringContainsString('401', $crawler->text());
+    }
+
+    public function testAnonymousUserCantAccessUsers():void
+    {
+        $client = static::createPantherClient();
+        $crawler = $client->request('GET', '/api/users');
+        $this->assertStringContainsString('401', $crawler->text());
+    }
+}
