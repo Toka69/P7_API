daeff32d65e03c64debe77f35f2bc068d82e2ba1
Matthias LEROUX <matthiasleroux@laposte.net>
2021-05-24 22:27:46 +0200

    Rename file.

--- tests/PantherTest.php
+++ tests/PantherTest.php
@@ -4,7 +4,7 @@ namespace App\Tests;
 
 use Symfony\Component\Panther\PantherTestCase;
 
-class FunctionalTest extends PantherTestCase
+class PantherTest extends PantherTestCase
 {
     public function testAnonymousUserCantAccessProducts():void
     {
